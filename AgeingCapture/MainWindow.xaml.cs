1c5b033d5f4dbb7c2cd1e6ba344dde8000c5b6a0
蒋鹏 <1319379673@qq.com>
2023-07-06 10:25:19 +0800

    first commit

---
+++ AgeingCapture/MainWindow.xaml.cs
@@ -0,0 +1,225 @@
+﻿using AgeingCapture.Models;
+using AutoCali.Infrastructure;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgeingCapture
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ChoosePath_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog.Multiselect = false; // 设置为 true 可以选择多个文件
+            openFileDialog.InitialDirectory = "C:\\"; // 设置对话框的初始目录
+            openFileDialog.Filter = "All Files (*.*)|*.*"; // 设置文件过滤器，限制可选文件类型
+
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                txtPath.Text = openFileDialog.FileName;
+                if (string.IsNullOrEmpty(txtPath.Text))
+                {
+                    System.Windows.Forms.MessageBox.Show("路径不允许为空！");
+                    return;
+                }                
+            }
+        }
+
+        private int SafeParseDecimal(string value,string ErrorMsg)
+        {
+            int result;
+            if (int.TryParse(value, out result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new FormatException(ErrorMsg);
+            }
+        }
+
+        private async void btn_Save_Click(object sender, RoutedEventArgs e)
+        {          
+            if (string.IsNullOrEmpty(txtRate.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("Capture路径不允许为空！");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtPath.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("Json文件路径不允许为空！");
+                return;
+            }
+            try
+            {
+                string jsonContent = File.ReadAllText(txtPath.Text);
+                var root = JsonConvert.DeserializeObject<AgeingParam>(jsonContent);
+                foreach (var data in root.Ageings)//循环每一个老化
+                {
+                    //读取master.ini中的DeviceModel
+                    if (!Directory.Exists(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(txtRate.Text), "Config")))
+                    {
+                        Directory.CreateDirectory(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(txtRate.Text), "Config"));
+                    }
+                    var ini = new IniFile(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(txtRate.Text), "Config\\Master.ini"));
+                    int DeviceModel = SafeParseDecimal(ini.IniReadValue("default", "DeviceModel"), "Master.ini文件中default节点下的DeviceModel值不正确，请检查！");
+                    //将Json文件中的DeviceModel修改成Master.ini中一致。
+                    data.DeviceModel = DeviceModel.ToString();
+                    string CapPath = txtRate.Text;
+                    string iniPath = "";
+                    //首先判断机器号，是20还是30
+                    switch (DeviceModel)
+                    {
+                        case (int)EnumDeviceModel.Mode1020S:
+                        case (int)EnumDeviceModel.Mode1020MS:
+                        case (int)EnumDeviceModel.Mode1020S2:
+                        case (int)EnumDeviceModel.Mode1020MS2:
+                            iniPath = Write20Ini(data);
+                            break;
+                        default:
+                            iniPath = Write30Ini(data);
+                            break;
+                    }
+                    data.Ini = iniPath;
+                    // 创建一个进程对象
+                    Process process = new Process();
+                    // 设置要执行的可执行文件路径
+                    process.StartInfo.FileName = CapPath;
+                    // 设置参数字符串
+                    process.StartInfo.Arguments = data.ToString();
+
+                    // 异步启动进程
+                    await Task.Run(() =>
+                    {
+                        // 启动进程
+                        process.Start();
+                        // 等待进程执行完毕
+                        process.WaitForExit();
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex + ex.StackTrace);
+                System.Windows.Forms.MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        public string Write20Ini(Ageing data)
+        {
+            var ct = data.CT;
+            string filePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(txtRate.Text), "Config\\auto1020.ini");
+            // 创建空文件
+            File.Create(filePath).Dispose();
+            var ini = new IniFile(filePath);
+            ini.IniWriteValue("CT", "FovType", ct.FovType.ToString());
+            ini.IniWriteValue("CT", "CTModeType", ct.CTModeType.ToString());
+            ini.IniWriteValue("CT", "ResolutionType", ct.ResolutionType.ToString());
+            ini.IniWriteValue("CT", "Teeth", ct.Teeth.ToString());
+            ini.IniWriteValue("CT", "Mar", ct.Mar.ToString());
+            ini.IniWriteValue("CT", "Smart3DPano", ct.Smart3DPano.ToString());
+            ini.IniWriteValue("CT", "FastScan", ct.FastScan.ToString());
+            ini.IniWriteValue("CT", "PatientType", ct.PatientType.ToString());
+            ini.IniWriteValue("CT", "KV", ct.KV.ToString());
+            ini.IniWriteValue("CT", "MA", ct.MA.ToString());
+            var Pano = data.Pano;
+            ini.IniWriteValue("Pano", "TMJ", Pano.TMJ.ToString());
+            ini.IniWriteValue("Pano", "Autofocus", Pano.Autofocus.ToString());
+            ini.IniWriteValue("Pano", "TrajectoryType", Pano.TrajectoryType.ToString());
+            ini.IniWriteValue("Pano", "PatientType", Pano.PatientType.ToString());
+            ini.IniWriteValue("Pano", "KV", Pano.KV.ToString());
+            ini.IniWriteValue("Pano", "MA", Pano.MA.ToString());
+            var CEPH = data.CEPH;
+            ini.IniWriteValue("CEPH", "CEPHModeType", CEPH.CEPHModeType.ToString());
+            ini.IniWriteValue("CEPH", "FastScan", CEPH.FastScan.ToString());
+            ini.IniWriteValue("CEPH", "PatientType", CEPH.PatientType.ToString());
+            ini.IniWriteValue("CEPH", "KV", CEPH.KV.ToString());
+            ini.IniWriteValue("CEPH", "MA", CEPH.MA.ToString());
+            return filePath;
+
+        }
+
+        public string Write30Ini(Ageing data)
+        {
+            var ct = data.CT;
+            string filePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(txtRate.Text), "Config\\auto1030.ini");
+            // 创建空文件
+            File.Create(filePath).Dispose();
+            var ini = new IniFile(filePath);
+            ini.IniWriteValue("CT", "AutoScanFov", ct.AutoScanFov.ToString());
+            ini.IniWriteValue("CT", "CTModeType", ct.CTModeType.ToString());
+            ini.IniWriteValue("CT", "Teeth", ct.Teeth.ToString());
+            ini.IniWriteValue("CT", "Mar", ct.Mar.ToString());
+            ini.IniWriteValue("CT", "Smart3DPano", ct.Smart3DPano.ToString());
+            ini.IniWriteValue("CT", "FastScan", ct.FastScan.ToString());
+            ini.IniWriteValue("CT", "PatientType", ct.PatientType.ToString());
+            ini.IniWriteValue("CT", "KV", ct.KV.ToString());
+            ini.IniWriteValue("CT", "MA", ct.MA.ToString());
+            var Pano = data.Pano;
+            ini.IniWriteValue("Pano", "AutoScanPanoMode", Pano.AutoScanPanoMode.ToString());
+            ini.IniWriteValue("Pano", "TrajectoryType", Pano.TrajectoryType.ToString());
+            ini.IniWriteValue("Pano", "PanoROI", Pano.PanoROI.ToString());
+            ini.IniWriteValue("Pano", "Teeth", Pano.Teeth.ToString());
+            ini.IniWriteValue("Pano", "PatientType", Pano.PatientType.ToString());
+            ini.IniWriteValue("Pano", "KV", Pano.KV.ToString());
+            ini.IniWriteValue("Pano", "MA", Pano.MA.ToString());
+            var CEPH = data.CEPH;
+            ini.IniWriteValue("CEPH", "CEPHModeType", CEPH.CEPHModeType.ToString());
+            ini.IniWriteValue("CEPH", "FastScan", CEPH.FastScan.ToString());
+            ini.IniWriteValue("CEPH", "PatientType", CEPH.PatientType.ToString());
+            ini.IniWriteValue("CEPH", "KV", CEPH.KV.ToString());
+            ini.IniWriteValue("CEPH", "MA", CEPH.MA.ToString());
+            return filePath;
+        }
+
+        private void btn_newFile_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog.Multiselect = false; // 设置为 true 可以选择多个文件
+            openFileDialog.InitialDirectory = "C:\\"; // 设置对话框的初始目录
+            openFileDialog.Filter = "All Files (*.*)|*.*"; // 设置文件过滤器，限制可选文件类型
+
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                txtRate.Text = openFileDialog.FileName;
+                if (string.IsNullOrEmpty(txtRate.Text))
+                {
+                    System.Windows.Forms.MessageBox.Show("路径不允许为空！");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ AgeingCapture/Models/AgeingParam.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeingCapture.Models
+{
+    enum EnumDeviceModel
+    {
+        Mode1020S = 1020,//1020S，老界面
+        Mode1020MS = 1021,//1020MS，老界面
+        Mode1020S2 = 1025,//1020S，强检界面
+        Mode1020MS2 = 1026,//1020MS，强检界面
+        Mode1030XS = 1030,//1030XS
+        Mode1030XE = 1031,//1030XE
+        Mode1030pro = 1035,//1030pro
+        Mode1030pro_16x11 = 1036,//1030pro_16x11
+        Mode1040 = 1040,
+        Mode1040pro = 1045,
+    }
+    public class AgeingParam
+    {
+        public List<Ageing> Ageings { get; set; }
+    }
+
+    public class Ageing
+    {
+        [JsonProperty("-auto")]
+        public string Auto { get; set; }
+
+        [JsonProperty("-ini")]
+        public string Ini { get; set; }
+
+        [JsonProperty("-fov")]
+        public int Fov { get; set; }
+
+        [JsonProperty("-number")]
+        public string Number { get; set; }
+
+        [JsonProperty("-name")]
+        public string Name { get; set; }
+
+        [JsonProperty("-id")]
+        public string Id { get; set; }
+
+        [JsonProperty("-sex")]
+        public int Sex { get; set; }
+
+        [JsonProperty("-age")]
+        public int Age { get; set; }
+
+        [JsonProperty("-birth")]
+        public string Birth { get; set; }
+
+        [JsonProperty("-ph")]
+        public string Ph { get; set; }
+
+        [JsonProperty("-doc")]
+        public string Doc { get; set; }
+
+        [JsonProperty("-desc")]
+        public string Desc { get; set; }
+
+        [JsonProperty("-stuid")]
+        public string Stuid { get; set; }
+
+        [JsonProperty("-dicompath")]
+        public string DicomPath { get; set; }
+
+        [JsonProperty("-Locale")]
+        public string Locale { get; set; }
+
+        [JsonProperty("-hostName")]
+        public string HostName { get; set; }
+
+        [JsonProperty("-port")]
+        public int Port { get; set; }
+
+        [JsonProperty("-userName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("-password")]
+        public string Password { get; set; }
+
+        [JsonProperty("-DeviceModel")]
+        public string DeviceModel { get; set; }
+
+        public CTParams CT { get; set; }
+        public PanoParams Pano { get; set; }
+        public CephParams CEPH { get; set; }
+
+        public override string ToString()
+        {
+            return $"-auto{Auto} -ini{Ini} -fov{Fov} -number{Number} -name{Name} -id{Id} -sex{Sex} -age{Age} -birth{Birth} -ph{Ph} -doc{Doc} -desc{Desc} -stuid{Stuid} -dicompath{DicomPath} -Locale{Locale} -hostName{HostName} -port{Port} -userName{UserName} -password{Password} -devicemodel{DeviceModel}";
+        }
+    }
+
+    public class CTParams
+    {
+        public int AutoScanFov { get; set; }
+        public int CTModeType { get; set; }
+        public int Teeth { get; set; }
+        public int Mar { get; set; }
+        public int Smart3DPano { get; set; }
+        public int FastScan { get; set; }
+        public int PatientType { get; set; }
+        public int KV { get; set; }
+        public int MA { get; set; }
+        public int FovType { get; set; }
+        public int ResolutionType { get; set; }
+    }
+
+    public class PanoParams
+    {
+        public int AutoScanPanoMode { get; set; }
+        public int TrajectoryType { get; set; }
+        public int PanoROI { get; set; }
+        public string Teeth { get; set; }
+        public int PatientType { get; set; }
+        public int KV { get; set; }
+        public int MA { get; set; }
+        public int TMJ { get; set; }
+        public int Autofocus { get; set; }
+    }
+
+    public class CephParams
+    {
+        public int CEPHModeType { get; set; }
+
+        public int FastScan { get; set; }
+        public int PatientType { get; set; }
+        public int KV { get; set; }
+        public int MA { get; set; }
+    }
+}
